ffbc2f3af0ea290fecdbd976cdb073df66cf9994
Alexander Duque <joseduquee@gmail.com>
2022-06-01 17:12:00 +0200

    initial project

---
+++ src/components/MainNav.vue
@@ -0,0 +1,54 @@
+<template>
+  <header class="w-full text-sm">
+    <div class="fixed top-0 left-0 w-full h-16 bg-white">
+      <div
+        class="
+          flex flex-nowrap
+          h-full
+          px-8
+          mx-auto
+          border-b border-solid border-brand-gray-1
+        "
+      >
+        <a :href="url" class="flex items-center h-full text-xl">{{ company }}</a>
+        <nav class="h-full ml-12">
+          <ul class="flex h-full p-0 m-0 list-none">
+            <li class="h-full">
+              <a href="" class="flex items-center h-full py-2.5">Teams</a>
+            </li>
+            <li class="h-full ml-9">
+              <a href="" class="flex items-center h-full py-2.5">Locations</a>
+            </li>
+            <li class="h-full ml-9">
+              <a href="" class="flex items-center h-full py-2.5">Life at Leax IT</a>
+            </li>
+            <li class="h-full ml-9">
+              <a href="" class="flex items-center h-full py-2.5">How we hire</a>
+            </li>
+            <li class="h-full ml-9">
+              <a href="" class="flex items-center h-full py-2.5">Students</a>
+            </li>
+            <li class="h-full ml-9">
+              <a href="" class="flex items-center h-full py-2.5">Jobs</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "MainNav",
+  setup() {
+    
+    
+    return {
+      company: "Leax IT Careers",
+      url: "https://careers.google.com"
+    };
+  },
+};
+</script>
+
